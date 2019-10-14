e1ab24ad2ab06b25a744c26e9bc9e28ca21a72c1
gilperopiola <ferra.main@gmail.com>
2019-10-14 15:31:51 -0300

    change name length

--- database/schema_queries.go
+++ database/schema_queries.go
@@ -3,7 +3,7 @@ package database
 var createEntitiesTableQuery = `
 CREATE TABLE IF NOT EXISTS entities (
 	id int UNIQUE NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	name varchar(2055) NOT NULL,
 	description varchar(2055) NOT NULL,
 	kind varchar(255) NOT NULL,
 	importance int NOT NULL,
